ea571fe3e747ba66123ca57d348fc776603bdfcc
nkrathod <nkrsoft.tech@gmail.com>
2019-11-14 02:19:59 +0530

    Updates

---
+++ precache-manifest.cff0b764553085809404d2f83cb67115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dddcacb3fdb9145eea4106e9a50ad7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8666873b83ba9f1e1d",
+    "url": "/static/css/main.395fefcc.chunk.css"
+  },
+  {
+    "revision": "ccf5ea67995ab00cbce2",
+    "url": "/static/js/2.180a9817.chunk.js"
+  },
+  {
+    "revision": "9b8666873b83ba9f1e1d",
+    "url": "/static/js/main.3103f6d9.chunk.js"
+  },
+  {
+    "revision": "f25bafd5302e36aba9d6",
+    "url": "/static/js/runtime-main.814194fe.js"
+  }
+]);
